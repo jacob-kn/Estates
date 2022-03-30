cea09b564ebda05195b5933220ac7c989501c0a2
jacob <jacobn0223@outlook.com>
2022-03-29 19:30:03 -0600

    Added frontend for Listed Properties and Saved Properties

--- frontend/src/pages/ListedProperties.jsx
+++ frontend/src/pages/ListedProperties.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import PropertyCard from '../components/PropertyCard.jsx'
 
 function ListedProperties () {
-  return <div>ListedProperties</div>
+  let module = require('./tmpProps.js')
+  let tmpProps = module.tmpProps.filter(
+    prop => prop.seller.email === 'filler@gmail.com'
+  )
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>Your Listings</h1>
+      </section>
+
+      <section className='content'>
+        <div className='properties'>
+          {tmpProps.map(property => (
+            <PropertyCard
+              key={property._id}
+              property={property}
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default ListedProperties
--- frontend/src/pages/SavedProperties.jsx
+++ frontend/src/pages/SavedProperties.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import PropertyCard from '../components/PropertyCard.jsx'
 
 function SavedProperties () {
-  return <div>SavedProperties</div>
+  let module = require('./tmpProps.js')
+  let tmpProps = module.tmpProps.slice(2, 5)
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>Saved Properties</h1>
+      </section>
+
+      <section className='content'>
+        <div className='properties'>
+          {tmpProps.map(property => (
+            <PropertyCard key={property._id} property={property} />
+          ))}
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default SavedProperties
