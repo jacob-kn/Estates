52059ad552bba3cde1e18357fc2d21f0db14f80d
jacob <jacobn0223@outlook.com>
2022-04-14 14:19:19 -0600

    Added agreements store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
 import propReducer from '../features/addProperties/addPropertySlice'
+import agreementReducer from '../features/agreements/agreementsSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    addProperties: propReducer
+    addProperties: propReducer,
+    addAgreement: agreementReducer
   }
 })
