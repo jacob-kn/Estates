37de1999d6ea4d118a80ebb6d0d24dde93ed3a8c
Cheyenne Goh <cheyenne.goh1@ucalgary.ca>
2022-04-14 00:12:43 -0600

    agreement and user page updates

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import CommentsCard from '../components/Comments'
+import { FaPaperPlane } from "react-icons/fa";
 
 function User () {
   let { id } = useParams()
@@ -13,6 +15,8 @@ function User () {
   let tmpSeller = module.tmpSeller
 
   const seller = tmpSeller.filter(tmpSeller => tmpSeller._id == id)[0]
+
+  const [comment, setComment] = useState('')
 
   let headline = "Seller"
   if (seller.isRealtor) {
@@ -29,24 +33,58 @@ function User () {
     window.location.href=mailto
   }
 
+  const onComment = e => {
+    setComment(e.target.value)
+  }
+
+  const onSubmit = e => {}
+
   return (
   <div className='content'>
     <h1>{seller.email}</h1>
     <h2>{headline}</h2>
+    {user && user.type === 'buyer' ? (
+      <>
+        <br/>
+        <div style={{width:"12rem", margin:"auto"}}>
+          <button type='submit' className='btn btn-block' onClick={composeEmail}>
+          <FaPaperPlane /> Send an Email
+          </button>        
+        </div>
+      </>
+    ) : (<></>)}
     {seller.isRealtor ? (
       <>
         <br/>
         <label htmlFor='Comment'>
           <CommentsCard comment={seller.Comments} />
         </label>
-      </>
-    ) : (<></>)}
-    {user && user.type === 'buyer' ? (
-      <>
-        <br/>
-        <button type='submit' className='btn btn-block' onClick={composeEmail}>
-        Send an Email
-        </button>
+        {user && user.type === 'buyer' ? (
+          <>
+            <br/>
+            <section className='form'>
+              <form onSubmit={onSubmit}>
+                <div className='form-group'>
+                  <label>Leave a Comment</label>
+                  <textarea
+                    className='form-control'
+                    id='legal-name'
+                    value={comment}
+                    placeholder='Write your own review here...'
+                    onChange={onComment}
+                  />
+                </div>
+                <button 
+                  type='submit' 
+                  className='btn btn-block' 
+                  onClick={composeEmail}
+                >
+                  Add Comment
+                </button>
+              </form>
+            </section>
+          </>
+        ) : (<></>)}
       </>
     ) : (<></>)}
   </div>
